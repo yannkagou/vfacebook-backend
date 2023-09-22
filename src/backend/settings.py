2c090bbf23d0e651081217f47b83211e8ba422a9
Yannick KAGOU <112559433+yannkagou@users.noreply.github.com>
2023-09-22 12:18:11 +0200

    Update the backend for the last time

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -107,7 +107,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
